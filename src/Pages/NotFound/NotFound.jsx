d468204c68d31d668629fd19513679a9177e68ca
unknown <emanuelpinedo7@gmail.com>
2025-05-05 14:34:39 -0300

    cambio ruta img

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-semibold mb-4">404 || NOT FOUND || te re fuiste crack</h1>
-      <img src="../../../public/yoese.jpg" className="w-110 h-auto mb-4" alt="Imagen no encontrada" />
+      <img src="/yoese.jpg" className="w-110 h-auto mb-4" alt="Imagen no encontrada" />
       <button 
         onClick={() => navigate("/")} 
         className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition"
