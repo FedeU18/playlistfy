b9f7190551eee273e881d93f36e9608112894f41
unknown <emanuelpinedo7@gmail.com>
2025-05-05 14:31:42 -0300

    estilo al notfound kkkk

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -3,11 +3,18 @@ import { useNavigate } from "react-router";
 
 const NotFound = () => {
   const navigate = useNavigate();
+  
   return (
-    <>
-      <h1>404 || NOT FOUND || te re fuiste crack</h1>
-      <button onClick={() => navigate("/")}>Volver a Home</button>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-semibold mb-4">404 || NOT FOUND || te re fuiste crack</h1>
+      <img src="../../../public/yoese.jpg" className="w-110 h-auto mb-4" alt="Imagen no encontrada" />
+      <button 
+        onClick={() => navigate("/")} 
+        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition"
+      >
+        Volver a Home
+      </button>
+    </div>
   );
 };
 
